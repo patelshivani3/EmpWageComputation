40f80f8218de22d5ac3aeab753422ab6a41e6197
patelshivani3 <shivpatel3001@gmail.com>
2022-12-20 11:04:33 +0530

    Adding While Loop UC6

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -16,40 +16,40 @@ namespace EmpWageComputation
             const int FULL_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
-            int empHrs = 0, empWage = 0 ,totalWage = 0,day = 0;
+            const int MAX_WORKING_HRS = 100;
+            int empHrs = 0, empWage = 0 ,totalWage = 0, day =1,totalHrs = 0;
 
             //UC1 Employee is present or not
             Random random = new Random();
-            int empInput = random.Next(0,3);
+           
 
             //UC5 Using for loop
-            for(day = 1; day <= MAX_WORKING_DAYS; day++) 
+            while( day <= MAX_WORKING_DAYS && totalHrs<= MAX_WORKING_HRS ) 
             {
+                int empInput = random.Next(0, 3);
                 //UC4 Employee wage using switch case
                 switch (empInput)
                 {
                     case FULL_TIME:
                         empHrs = 8;
-                        Console.WriteLine("\nEmployee is FullTime Present");
                         break;
 
                     case PART_TIME:
                         empHrs = 4;
-                        Console.WriteLine("\nEmployee is PartTime Present");
                         break;
 
                     default:
                         empHrs = 0;
-                        Console.WriteLine("\nEmployee is Absent");
                         break;
                 }
                 //UC2 Employee daily wage 
                 empWage = EMP_RATE_PER_HR * empHrs;
                 totalWage = totalWage + empWage;
-                Console.WriteLine("day {0} employee wage  is :{1} " ,day, empWage);
+                totalHrs += empHrs;
+                day++;
             }
             
-            Console.WriteLine("Total Wage for 20 days is :{0} ", totalWage);
+            Console.WriteLine("Total Wage for 20 days and {0} Hours is : {1}",totalHrs,totalWage);
             Console.ReadLine();
 
         }
