85e317edb8f62471ba0e43e2953171bd4addb69b
patelshivani3 <shivpatel3001@gmail.com>
2022-12-20 08:29:03 +0530

    Adding Employee Daily Wage UC2

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -12,16 +12,25 @@ namespace EmpWageComputation
         {
             Console.WriteLine("\nWelcome to Employee Wage Computation");
             const int IS_PRESENT = 0;
+            const int EMP_RATE_PER_HR = 20;
+            int empHrs = 0, empWage = 0;
 
             Random random = new Random();
             int empInput = random.Next(0,2);
-            
+
             //UC1 Check employee is present or absent
             if (IS_PRESENT == empInput)
+            {
                 Console.WriteLine("\nEmployee is Present");
+                empHrs = 8;
+            }
             else
+            {
                 Console.WriteLine("\nEmployee is Absent");
-            
+                empHrs = 0;
+            }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee daily wage is : "+empWage);
             Console.ReadLine();
         }
     }
