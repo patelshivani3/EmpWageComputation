94360d15009af3793957981949ee285d91b3ff7b
patelshivani3 <shivpatel3001@gmail.com>
2022-12-23 16:24:51 +0530

    Updating The Program With Class Method & Variable

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -8,23 +8,27 @@ namespace EmpWageComputation
 {
     internal class Program
     {
+        const int PART_TIME = 1;
+        const int FULL_TIME = 2;
+        const int EMP_RATE_PER_HR = 20;
+        const int MAX_WORKING_DAYS = 20;
+        const int MAX_WORKING_HRS = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Employee Wage Computation");
-            
-            const int PART_TIME = 1;
-            const int FULL_TIME = 2;
-            const int EMP_RATE_PER_HR = 20;
-            const int MAX_WORKING_DAYS = 20;
-            const int MAX_WORKING_HRS = 100;
-            int empHrs = 0, empWage = 0 ,totalWage = 0, day =1,totalHrs = 0;
+            CalculateWage();       
+            Console.ReadLine();
+        }
+        public static void CalculateWage()
+        {
+            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalHrs = 0;
 
             //UC1 Employee is present or not
             Random random = new Random();
-           
+
 
             //UC5 Using for loop
-            while( day <= MAX_WORKING_DAYS && totalHrs<= MAX_WORKING_HRS ) 
+            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int empInput = random.Next(0, 3);
                 //UC4 Employee wage using switch case
@@ -48,12 +52,8 @@ namespace EmpWageComputation
                 totalHrs += empHrs;
                 day++;
             }
-            
-            Console.WriteLine("Total Wage for 20 days and {0} Hours is : {1}",totalHrs,totalWage);
-            Console.ReadLine();
-
+            Console.WriteLine("Total Wage for 20 days and {0} Hours is : {1}", totalHrs, totalWage);
         }
-
 
     }
 }
