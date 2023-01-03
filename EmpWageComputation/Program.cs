a8c4794d1f8d3c52f6431378df44248257ebd1f0
patelshivani3 <shivpatel3001@gmail.com>
2023-01-03 14:49:37 +0530

    Adding Total Wage For Each Company

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -13,47 +13,17 @@ namespace EmpWageComputation
         
         static void Main(string[] args)
         {
-            Console.WriteLine("\nWelcome to Employee Wage Computation\n");
-            CalculateWage("dell", 32, 25, 70);
-            CalculateWage("hp", 30, 25, 75);
-            CalculateWage("lenova", 28, 25, 60);
+            EmpWageBuilderObject dell = new EmpWageBuilderObject("Dell", 32, 25, 70);
+            dell.CalculateWage();
+            Console.WriteLine(dell.ToString());
+            EmpWageBuilderObject hp = new EmpWageBuilderObject("HP", 30, 25, 75);
+            hp.CalculateWage();
+            Console.WriteLine(hp.ToString());
+            EmpWageBuilderObject lenova = new EmpWageBuilderObject("Lenova", 28, 25, 60);
+            lenova.CalculateWage();
+            Console.WriteLine(lenova.ToString());
             Console.ReadLine();
+
         }
-        public static void CalculateWage(string company, int empRatePerHr, int maxWorkingDays, int maxWorkingHrs)
-        {
-            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalHrs = 0;
-
-            //UC1 Employee is present or not
-            Random random = new Random();
-
-
-            //UC5 Using for loop
-            while (day <= maxWorkingDays && totalHrs <= maxWorkingHrs)
-            {
-                int empInput = random.Next(0, 3);
-                //UC4 Employee wage using switch case
-                switch (empInput)
-                {
-                    case FULL_TIME:
-                        empHrs = 8;
-                        break;
-
-                    case PART_TIME:
-                        empHrs = 4;
-                        break;
-
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                //UC2 Employee daily wage 
-                empWage = empRatePerHr * empHrs;
-                totalWage = totalWage + empWage;
-                totalHrs += empHrs;
-                day++;
-            }
-            Console.WriteLine("Total Wage of {0} for 20 days and {1} Hours is : {2}",company, totalHrs, totalWage);
-        }
-
     }
 }
